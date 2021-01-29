aef4b777a4a9ccfe472c21e11b7870ccae0c929c
alextagun <alextagun@gmail.com>
2021-01-29 23:51:21 +0300

    add player prefab

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CameramanMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour {
     [SerializeField] private Camera _camera = null;
     [SerializeField] private float _speed = 0f;
     [SerializeField] private float _speedRotation = 0f;
@@ -12,11 +12,6 @@ public class CameramanMovement : MonoBehaviour {
     [SerializeField] private int _jumpFrameTime = 0;
     [SerializeField] private CharacterController _characterController = null;
 
-
-
-    public float JumpSpeed => _jumpSpeed;
-    public int JumpFrameTime => _jumpFrameTime;
-    // вращение камеры
     private float mouseDeltaX = 0f;
     private float mouseDeltaY = 0f;
     private Quaternion startRotation = Quaternion.identity;
@@ -39,9 +34,52 @@ public class CameramanMovement : MonoBehaviour {
         _camera.transform.rotation = startRotation * verticalRotation * horizontalRotarion;
     }
 
-    public void Move(Vector3 vel) {
+    private void Move(Vector3 vel) {
         vel.x *= _speed * Time.deltaTime;
         vel.z *= _speed * Time.deltaTime;
         _characterController.Move(vel);
+    }
+    
+    public bool CanMove = false;
+    public bool CanShoot = false;
+
+    private void Update()
+    {
+        // движение камеры и поворот player
+        Rotate(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 velocity = transform.right * x + transform.forward * z;
+        velocity.y -= 9.8f * Time.deltaTime;
+
+        if (_characterController.isGrounded)
+        {
+            // velocity.y = 0;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // velocity.y = _cameramanMovement.JumpSpeed * Time.deltaTime;
+                // velocity.y += Mathf.Sqrt(_cameramanMovement.JumpSpeed * -2f * -9.8f);
+                StartCoroutine(Jump());
+            };
+        }
+        //velocity.y -= 9.8f * Time.deltaTime;
+        Move(velocity);
+        
+    }
+
+
+  
+
+    private IEnumerator Jump() {
+        var k = 0.01f;
+        for (int i = 0; i < _jumpFrameTime; i++) {
+            Vector3 velocity = Vector3.zero;
+            velocity.y += Mathf.Sqrt((_jumpSpeed - k) * -2f * -9.8f);
+            k += k;
+            Move(velocity);
+            yield return null;
+        }
     }
 }
