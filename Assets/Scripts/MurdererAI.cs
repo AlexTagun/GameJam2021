3de78281db3ec2b50a58b0d6925a43a0374bf000
AlexandrKutsar <alexandrkutsar@gmail.com>
2021-01-29 23:16:00 +0300

    импорт nav mesh + пробное движение маньяка

---
+++ Assets/Scripts/MurdererAI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MurdererAI : MonoBehaviour
+{
+    [SerializeField] private float timeToChangeGoalPosition = 3f;
+    [SerializeField] private Transform playerTransform;
+
+    private NavMeshAgent navMesh;
+
+    private float curTime = 0f;
+
+    private void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+
+        if (curTime >= timeToChangeGoalPosition)
+        {
+            curTime = 0f;
+            navMesh.SetDestination(playerTransform.position);
+        }
+        else curTime += Time.deltaTime;
+    }
+}
