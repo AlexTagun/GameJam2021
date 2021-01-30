a8a745a79007b2668ef9b113fcd70195951b211c
alextagun <alextagun@gmail.com>
2021-01-30 20:16:27 +0300

    make flashlight visible from distance

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -1,9 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Flashlight : MonoBehaviour
-{
+public class Flashlight : MonoBehaviour {
+    [SerializeField] private Light pointLight;
+    
+    private Transform _player;
+    
+    private void Start() {
+        _player = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void Update() {
+        float distance = Vector3.Distance(_player.position, transform.position);
+        distance = Mathf.Clamp(distance, 0, 80);
+        pointLight.intensity = Mathf.Lerp(5, 200, distance / 80);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
