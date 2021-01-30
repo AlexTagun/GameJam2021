b0bbfb09cdde74179e130384ce2e166bdb1f44a8
alextagun <alextagun@gmail.com>
2021-01-30 15:30:29 +0300

    add murderer ai

--- Assets/Scripts/MurdererAI.cs
+++ Assets/Scripts/MurdererAI.cs
@@ -7,24 +7,68 @@ public class MurdererAI : MonoBehaviour
 {
     [SerializeField] private float timeToChangeGoalPosition = 3f;
     [SerializeField] private Transform playerTransform;
+    [SerializeField] private float ScanFrequency;
+    [SerializeField] private float ScanRadius;
+    [SerializeField] private float AttackRadius;
+    [SerializeField] private Player player;
 
     private NavMeshAgent navMesh;
 
     private float curTime = 0f;
+    
+    private enum MurdererStates {
+        Roaming,
+        Following,
+        ReadyToAttack,
+        Attack,
+        Spotted,
+        Flee
+    }
+
+    private MurdererStates _curState;
+    private Coroutine _curCoroutine;
 
     private void Start()
     {
         navMesh = GetComponent<NavMeshAgent>();
+
+        _curState = MurdererStates.Roaming;
     }
 
     private void Update()
     {
 
-        if (curTime >= timeToChangeGoalPosition)
-        {
-            curTime = 0f;
-            navMesh.SetDestination(playerTransform.position);
+        // if (curTime >= timeToChangeGoalPosition)
+        // {
+        //     curTime = 0f;
+        //     navMesh.SetDestination(playerTransform.position);
+        // }
+        // else curTime += Time.deltaTime;
+
+        switch (_curState) {
+            case MurdererStates.Roaming:
+                if(_curCoroutine == null) _curCoroutine = StartCoroutine(ScanCoroutine());
+                // StartCoroutine(ScanCoroutine());
+                break;
+            case MurdererStates.Following:
+                navMesh.SetDestination(playerTransform.position);
+                if (Vector3.Distance(transform.position, player.transform.position) < AttackRadius) {
+                    Debug.Log("Kill");
+                }
+                break;
         }
-        else curTime += Time.deltaTime;
+    }
+
+    private IEnumerator ScanCoroutine() {
+        yield return new WaitForSeconds(ScanFrequency);
+        if ((ScanRadius + player.NoiseLevel * player.VisibilityLevel) >
+            Vector3.Distance(transform.position, player.transform.position)) {
+            Debug.Log("Player has found");
+            _curState = MurdererStates.Following;
+        } else {
+            Debug.Log("Player not found");
+        }
+
+        _curCoroutine = null;
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float NoiseLevel = 1;
+    public float VisibilityLevel = 20;
+}
