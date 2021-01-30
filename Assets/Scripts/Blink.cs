e4654469425593c07989b9030efaa5089b9d80c2
alextagun <alextagun@gmail.com>
2021-01-31 02:26:47 +0300

    add blink

---
+++ Assets/Scripts/Blink.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour {
+    [SerializeField] private Light red;
+    [SerializeField] private Light blue;
+
+    private void Start() {
+        red.gameObject.SetActive(false);
+        blue.gameObject.SetActive(true);
+        StartCoroutine(BlinkCoroutine());
+    }
+
+    private IEnumerator BlinkCoroutine() {
+        bool value = false;
+        while (true) {
+            value = !value;
+            red.gameObject.SetActive(value);
+            blue.gameObject.SetActive(!value);
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+}
