996c96f01bc0dcd4f830ce3f4e1dd888d787f510
AlexandrKutsar <alexandrkutsar@gmail.com>
2021-01-30 20:16:24 +0300

    добавил звуки ловушки

--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 
 public class Trap : MonoBehaviour
 {
+    [SerializeField] private AudioSource audioTrap = null;
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Player")) {
 
             var player = other.gameObject.GetComponent<Player>();
             if (player) player.DiedFromTraps();
+            audioTrap.Play();
             Debug.Log("TRAP");
         }
     }
