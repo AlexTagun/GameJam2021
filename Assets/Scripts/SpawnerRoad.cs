49a7689e172b54cf819508d7e859d52884d678b0
AlexandrKutsar <alexandrkutsar@gmail.com>
2021-01-31 13:35:13 +0300

    add spawn road

---
+++ Assets/Scripts/SpawnerRoad.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerRoad : MonoBehaviour
+{
+    [SerializeField] private List<WinPlace> winPlaces = new List<WinPlace>();
+
+
+    private bool isRoadSpawn = false;
+    void Start()
+    {
+        
+    }
+
+
+    private void SpawnRoad()
+    {
+        if (isRoadSpawn) return;
+        float maxDistance = 0;
+        WinPlace maxDistantPlace = null;
+        var playerPos = GameController.Instance.Player.transform.position;
+        foreach (var place in winPlaces)
+        {
+            var distance = Vector3.Distance(playerPos, place.Place.position);
+            if (distance > maxDistance)
+            {
+                maxDistance = distance;
+                maxDistantPlace = place;
+            }
+        }
+        maxDistantPlace.Activate();
+        isRoadSpawn = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SpawnRoad();
+        }
+    }
+}
--- Assets/Scripts/WinPlace.cs
+++ Assets/Scripts/WinPlace.cs
@@ -4,6 +4,49 @@ using UnityEngine;
 
 public class WinPlace : MonoBehaviour
 {
+    public Transform Place;
+
+    [SerializeField] private GameObject road;
+    [SerializeField] private GameObject blickPrefab;
+    [SerializeField] private Transform blickPosition;
+
+    private Transform blickTransform = null;
+    private Transform playerTransform = null;
+
+    private bool isActivate = false;
+
+    private void Start()
+    {
+        road.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!isActivate) return;
+        MoveBlickToPlayer();
+
+    }
+    public void Activate()
+    {
+        var blick = Instantiate(blickPrefab, blickPosition.position, Quaternion.identity);
+        blick.SetActive(true);
+        blickTransform = blick.transform;
+        playerTransform = GameController.Instance.Player.transform;
+        road.SetActive(true);
+        isActivate = true;
+    }
+
+    public void MoveBlickToPlayer()
+    {
+        var distance = Vector3.Distance(playerTransform.position, blickPosition.position);
+        if (distance > 100)
+        {
+            var direction = blickPosition.position - playerTransform.position;
+            blickTransform.position = playerTransform.position + direction.normalized * 100;
+        }
+        else blickTransform.position = blickPosition.position;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
