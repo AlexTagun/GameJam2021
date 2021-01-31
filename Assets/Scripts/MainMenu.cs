30ff149ee26dfb42635e73193dbef6950dbea669
alextagun <alextagun@gmail.com>
2021-01-31 16:57:13 +0300

    Add main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+    [SerializeField] private Button playButton;
+
+    [SerializeField] private GameObject[] ObjectsToShow;
+    
+    private ColorAdjustments _colorAdjustments;
+    private void Awake() {
+        playButton.onClick.AddListener(OnPlay);
+    }
+
+    private void Start() {
+        var volume = GameObject.Find("Global Volume").GetComponent<Volume>();
+        volume.profile.TryGet(out _colorAdjustments);
+    }
+
+    private void OnPlay() {
+        for (int i = 0; i < ObjectsToShow.Length; i++) {
+            ObjectsToShow[i].SetActive(true);
+        }
+        
+        LeanTween.value(gameObject, -100f, 0, 2f)
+            .setOnUpdate(f => {
+                _colorAdjustments.contrast.value = f;
+            });
+        
+        gameObject.SetActive(false);
+    }
+}
