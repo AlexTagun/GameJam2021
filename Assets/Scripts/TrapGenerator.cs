e5df3840145c287b7c44db1184c5088c23edbc15
alextagun <alextagun@gmail.com>
2021-01-31 15:50:31 +0300

    add trap generator

---
+++ Assets/Scripts/TrapGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class TrapGenerator : MonoBehaviour {
+    public int TrapsCount = 10;
+    public GameObject TrapPrefab;
+    private void Start() {
+        float cellSize = 1000f / TrapsCount;
+        for (int i = 0; i < TrapsCount - 1; i++) {
+            for (int j = 0; j < TrapsCount - 1; j++) {
+                var obj = Instantiate(TrapPrefab, transform);
+                Vector3 pos = new Vector3(Random.Range(cellSize * i, cellSize * i + cellSize), 0, Random.Range(cellSize * j, cellSize * j + cellSize));
+                obj.transform.position = pos;
+            }
+        }
+    }
+}
