12d49347f2f563367c24f5a83a41a354b84eb50f
AlexandrKutsar <alexandrkutsar@gmail.com>
2021-01-31 22:19:17 +0300

    добавил перемещение фонарика за игроком когда идет поиск фонаря

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -7,13 +7,27 @@ public class Flashlight : MonoBehaviour {
     [SerializeField] private Light pointLight;
     
     private Transform _player;
+    private Transform _tranform;
+    private Vector3 flashlightPosition;
     
     private void Start() {
         _player = GameObject.FindWithTag("Player").transform;
+        _tranform = transform;
+        flashlightPosition = transform.position;
     }
 
     private void Update() {
+
         float distance = Vector3.Distance(_player.position, transform.position);
+
+        if (distance > 80)
+        {
+            var direction = flashlightPosition - _player.position;
+            _tranform.position = _player.position + direction.normalized * 100;
+        }
+        else _tranform.position = flashlightPosition;
+
+        distance = Vector3.Distance(_player.position, transform.position);
         distance = Mathf.Clamp(distance, 0, 80);
         pointLight.intensity = Mathf.Lerp(5, 200, distance / 80);
     }
